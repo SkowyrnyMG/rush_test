6aa576ccdc0eb72ae7dd38cbe612ebf806c81fbf
Mateusz Gruźla <mateusz.gruzla@sobre.pl>
2021-09-30 22:18:52 +0200

    Create multipe repo deploy settings

---
+++ apps/admin-panel/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {Thing} from '@shared/components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Thing/>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
